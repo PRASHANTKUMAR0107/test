a5271f067ff6fca3e5f8c8277ace166e31a0107b
Prashant Kumar <prashantkumar010704@gmail.com>
2023-04-02 01:50:18 +0530

    hello

--- index.js
+++ index.js
@@ -1,11 +1,96 @@
 const express = require("express");
+const { google } = require("googleapis");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-const app =express();
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
-app.get("/",function(req,res){
-    res.send("working .........");
-})
+app.get("/", async(req, res) => {
 
-app.listen(process.env.PORT||4000,function(){
-    console.log("hello...")
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1RQxUwl37mQGZCDVlKoTUWcrl3EIJzT0LLKfeztuCaK8";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1",
+  });
+  res.send(getRows.data);
+});
+
+app.post("/", async (req, res) => {
+  const { name,age,gender,phone_number,alternate_phone_number,email,address,medical_history,experience,last_gym_name,requirements,transaction,duration,currDate } = req.body;
+
+  // console.log(req.body);
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1RQxUwl37mQGZCDVlKoTUWcrl3EIJzT0LLKfeztuCaK8";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+
+
+  // Write row(s) to spreadsheet
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:B",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[name,age,gender,phone_number,alternate_phone_number,email,address,medical_history,experience,last_gym_name,requirements,transaction,duration,currDate]],
+    },
+  });
+
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1",
+  });
+
+  res.send(getRows.data)
+});
+
+app.listen(process.env.PORT || 5000,function(){
+    console.log("running on port 5000...");
 })
